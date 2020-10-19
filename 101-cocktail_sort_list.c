e202c931a86bc299d8d400d538cef2f8fed925dd
Ana-Morales <moralesanaruth@gmail.com>
2020-10-19 17:01:38 +0000

    change order to initialize variable

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -30,11 +30,12 @@ void swap_func(listint_t **list, listint_t *node)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *current = *list;
+	listint_t *current;
 	int flag = 1;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
+	current = *list;
 	while (flag > 0)
 	{
 		flag = 0;
